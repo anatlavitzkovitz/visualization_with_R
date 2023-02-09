4ab7723375ac51c8fd7c8cad84304a5ffa833707
anat_li <anat.lavi.itzkovitz@hazera.com>
2023-02-09 22:12:25 +0200

    adedd a genetic map drawing script

--- main.R
+++ main.R
@@ -1,3 +1,67 @@
+
+make_genetic_map<-function(directory,MyMap,marker_t1,marker_t2,SNPS){
+  
+  pdf(paste0(directory,"step2/genetic_maps/genetic_map_",marker_t1,marker_t2,"_",length(SNPS),".pdf"),10,7)
+  par(mfrow=c(13,1))
+  chromosomes<-c("chr_01","chr_02","chr_03","chr_04","chr_05","chr_06","chr_07","chr_08","chr_09","chr_10","chr_11","chr_12")
+  
+  for (i in 1:length(chromosomes)){
+    #print(i)
+    par(mar=c(0,3,0,2)) 
+    x=MyMap[MyMap$CHROMOSOME%in%chromosomes[i],]
+    x_subset=MyMap_subset[MyMap_subset$CHROMOSOME%in%chromosomes[i],]
+    t1<-which(x$LOCUS==marker_t1)
+    if (length(t1)==0){
+      t1<-0 }
+    
+    t2<-which(x$LOCUS==marker_t2)
+    if (length(t2)==0){
+      t2<-0 }
+    
+    #print(t1)
+    #print(t2)
+    plot(x=x$POSITION,y=rep(0,dim(x)[1]),
+         col = ifelse(1:nrow(x) == t1, "red", "black"),
+         pch = ifelse(1:nrow(x) == t1, 19, 19),
+         cex = ifelse(1:nrow(x) == t1, 2, 0.2),
+         axes=0,xaxt=NULL,xlim=c(0,max(MyMap$POSITION)+0.2))
+    par(new=TRUE)
+    
+    #PLOT SUBSET
+    plot(x_subset$POSITION,rep(0,dim(x_subset)[1]),col="blue",pch=19,cex=1.5,
+         axes=0,xaxt=NULL,xlim=c(0,max(MyMap$POSITION)+0.2))
+    par(new=TRUE)
+    
+    #ADD TRAIT MARKERS
+    plot(x=x$POSITION,y=rep(0,dim(x)[1]),
+         col = ifelse(1:nrow(x) == t2, "red", "black"),
+         pch = ifelse(1:nrow(x) == t2, 19, 19),
+         cex = ifelse(1:nrow(x) == t2, 2, 0.2),
+         axes=0,xaxt=NULL,xlim=c(0,max(MyMap$POSITION)+0.2))
+    
+    text(x$POSITION,y=rep(0,dim(x)[1]),labels=ifelse(1:nrow(x) == t1,marker_t1,NA),cex=1,pos = 1,offset = 1)
+    text(x$POSITION,y=rep(0,dim(x)[1]),labels=ifelse(1:nrow(x) == t2,marker_t2,NA),cex=1,pos = 1,offset = 1)
+    
+    mtext(text = paste0("chr",i),side=2,cex=0.8,las=2)
+    
+    
+    if (i==12){
+      par(mar=c(0,3,0,2)) 
+      plot(x=-1,y=-1,axes=0,xaxt=NULL,xlim=c(0,max(MyMap$POSITION)+0.2))
+      axis(side = 1,at=seq(0,max(MyMap$POSITION)+0.2,0.2),labels=c(seq(0,max(MyMap$POSITION)+0.2,0.2)*100),
+           tick = TRUE,outer=TRUE,line=-2.5)
+      #axis(side=2,labels=NA)#,tick = FALSE)
+      mtext(side=1,text="CM", outer = TRUE, cex = 0.6,line = -1.5,adj = 0.95)
+    }
+  }
+  
+  dev.off()
+  
+}
+
+
+################################
+
 chr_genetic_map<-function(directory, file_name){
   
   marker_tab<-read.csv(paste0(directory,'/',file_name,'.csv'))
